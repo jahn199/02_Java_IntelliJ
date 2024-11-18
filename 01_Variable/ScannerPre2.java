0650c6eab3b0687e204f78e90ec7500f71dc56fa
jahn199 <anj575981@gmail.com>
2024-11-18 16:50:00 +0900

    java project

---
+++ 01_Variable/ScannerPre2.java
@@ -0,0 +1,18 @@
+package com.kh;
+
+import java.util.Scanner;
+
+public class ScannerPre2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.
+        //String name 이름 입력 받기
+
+        //int age 나이 입력 받기
+
+        //String hobby 취미 입력 받기
+
+        //출력 System.out.println 4번 입력
+    }
+}
